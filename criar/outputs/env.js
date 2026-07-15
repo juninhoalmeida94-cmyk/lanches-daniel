ebdcd34f6353a93e2bfc40aa5696df6cd188350b
Paulo De Almeida Junior <juninhoalmeida94@gmail.com>
2026-07-15 03:37:28 -0300

    Configura Supabase

--- criar/outputs/env.js
+++ criar/outputs/env.js
@@ -1,4 +1,4 @@
 window.__ENV__ = {
-  SUPABASE_URL: "https://pfgsckdknujsriljeila.supabase.co/",
+  SUPABASE_URL: "https://pfgsckdknujsriljeila.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_IafCHAv4RDJNRFIKjQBqtA_zXYtdsf2"
 };
